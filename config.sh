e3777d049e774250e63969520907c87e5dda9acd
Huayi Wei <weihuayi@xtu.edu.cn>
2018-12-12 20:45:04 +0800

    update

--- config.sh
+++ config.sh
@@ -1,13 +1,43 @@
+# Fugitive.vim - a complement to command line git
 git submodule add http://github.com/tpope/vim-fugitive.git vim/bundle/fugitive
+
+# SnipMate aims to provide support for textual snippets, 
+# similar to TextMate or other Vim plugins like UltiSnips. 
 git submodule add https://github.com/msanders/snipmate.vim.git vim/bundle/snipmate
+
+# Surround.vim is all about "surroundings": parentheses, brackets, quotes, XML tags, and more.
+# `cs"'` change surround "" to ''
 git submodule add https://github.com/tpope/vim-surround.git vim/bundle/surround
 git submodule add https://github.com/tpope/vim-git.git vim/bundle/git
+
+# supertab: 自动补全 c-n
 git submodule add https://github.com/ervandew/supertab.git vim/bundle/supertab
+
+# Several modern GUI editors list your open buffers as tabs along the top or bottom of your screen
 git submodule add https://github.com/fholgado/minibufexpl.vim.git vim/bundle/minibufexpl
+
+# 
+# Command-T is a Vim plug-in that provides an extremely fast "fuzzy" mechanism for:
+# * Opening files and buffers
+# * Jumping to tags and help
+# * Running commands, or previous searches and commands
+#
+# with a minimal number of keystrokes.
 git submodule add https://github.com/wincent/Command-T.git vim/bundle/command-t
+
+# Ack 是 CLI 的全局搜索工具, 比Vim自带的grep快很多, 
+# 它可以通过Vim Quickfix窗口来提供代码搜索的结果.
 git submodule add https://github.com/mileszs/ack.vim.git vim/bundle/ack
+
+# 文件时光机, 可以查看文件编辑中的内容
 git submodule add https://github.com/sjl/gundo.vim.git vim/bundle/gundo
+
+# makegreen.vim is a vim (http://www.vim.org) plugin that runs make and shows the 
+# test run status with a red or green bar.
 git submodule add https://github.com/reinh/vim-makegreen vim/bundle/makegreen
+
+# 
+#
 git submodule add https://github.com/vim-scripts/The-NERD-tree.git vim/bundle/nerdtree
 git submodule add https://github.com/jistr/vim-nerdtree-tabs.git vim/bundle/vim-nerdtree-tabs
 git submodule add https://github.com/python-rope/ropevim.git vim/bundle/ropevim
